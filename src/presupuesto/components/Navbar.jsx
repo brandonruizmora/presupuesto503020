53c03c0bea14757db7e99f87da33cc724b10c7a5
BrandonRuizMora <brandon_ruizmor@outlook.es>
2024-09-06 20:42:17 -0600

    Added navbar navigation through years months and budget pages

--- src/presupuesto/components/Navbar.jsx
+++ src/presupuesto/components/Navbar.jsx
@@ -1,29 +1,44 @@
-import { NavLink } from "react-router-dom"
+import { Link, NavLink, useLocation, useParams } from "react-router-dom"
 
 export const Navbar = () => {
 
+    const { pathname } = useLocation();
+
+    const parts = pathname.split("/").filter(Boolean); // Divide la cadena por "/" y elimina los elementos vacíos
+    const year = parseInt(parts[0], 10); // Convierte el primer valor en número
+    const month = parseInt(parts[1], 10); // Convierte el segundo valor en número
+    const budget = parts[2]; // Convierte el segundo valor en número
+
+    console.log(pathname)
+
+    console.log(budget)
+
+    if (!isNaN(month)) {
+        console.log("acctive class")
+    }
+
     return (
-        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <nav className="navbar navbar-expand-md bg-body-tertiary">
             <div className="container-fluid">
                 <a className="navbar-brand" href="/">Presupuesto 50/30/20</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarNav">
-                    <ul className="navbar-nav">
-                        <li className="nav-item">
-                            <NavLink className="nav-link" to="/">Home</NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink className="nav-link" to="/config">Configuración</NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link disabled" href="#">Pricing</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link disabled" aria-disabled="true">Disabled</a>
-                        </li>
+                    <ul className="navbar-nav me-auto">
+                        <NavLink className="nav-item nav-link" to="/">Años</NavLink>
+                        {
+                            !isNaN(year)
+                            &&
+                            <Link className={!isNaN(month) ? "nav-item nav-link" : "nav-item nav-link active"} to={"/" + year}>{`meses del ${year}`}</Link>
+                        }
+                        {
+                            !isNaN(month)
+                            &&
+                            <Link className={budget ? "nav-item nav-link" : "nav-item nav-link active"} to={`/${year}/${month}`}>Presupuesto</Link>
+                        }
                     </ul>
+                    <NavLink className="nav-link" exact to="/config">Configuración</NavLink>
                 </div>
             </div>
         </nav>
