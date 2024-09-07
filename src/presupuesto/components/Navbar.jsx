280a9a76a0246a72d13fd735a6633cbf44dd431b
BrandonRuizMora <brandon_ruizmor@outlook.es>
2024-09-06 20:55:12 -0600

    Added months names on budget navlink

--- src/presupuesto/components/Navbar.jsx
+++ src/presupuesto/components/Navbar.jsx
@@ -1,21 +1,22 @@
-import { Link, NavLink, useLocation, useParams } from "react-router-dom"
+import { Link, NavLink, useLocation } from "react-router-dom"
 
 export const Navbar = () => {
 
     const { pathname } = useLocation();
 
+    const monthNames = {
+        1: "Enero", 2: "Febrero", 3: "Marzo", 4: "Abril", 5: "Mayo", 6: "Junio",
+        7: "Julio", 8: "Agosto", 9: "Septiembre", 10: "Octubre", 11: "Noviembre", 12: "Diciembre"
+    };
+
+    //let monthText = "";
+
     const parts = pathname.split("/").filter(Boolean); // Divide la cadena por "/" y elimina los elementos vacíos
     const year = parseInt(parts[0], 10); // Convierte el primer valor en número
     const month = parseInt(parts[1], 10); // Convierte el segundo valor en número
-    const budget = parts[2]; // Convierte el segundo valor en número
+    const budget = parts[2];
 
-    console.log(pathname)
-
-    console.log(budget)
-
-    if (!isNaN(month)) {
-        console.log("acctive class")
-    }
+    const monthText = monthNames[month] || "";
 
     return (
         <nav className="navbar navbar-expand-md bg-body-tertiary">
@@ -30,15 +31,15 @@ export const Navbar = () => {
                         {
                             !isNaN(year)
                             &&
-                            <Link className={!isNaN(month) ? "nav-item nav-link" : "nav-item nav-link active"} to={"/" + year}>{`meses del ${year}`}</Link>
+                            <Link className={!isNaN(month) ? "nav-item nav-link" : "nav-item nav-link active"} to={`/${year}`}>{`meses del ${year}`}</Link>
                         }
                         {
                             !isNaN(month)
                             &&
-                            <Link className={budget ? "nav-item nav-link" : "nav-item nav-link active"} to={`/${year}/${month}`}>Presupuesto</Link>
+                            <Link className={budget ? "nav-item nav-link" : "nav-item nav-link active"} to={`/${year}/${month}`}>{`Presupuesto de ${monthText}`}</Link>
                         }
                     </ul>
-                    <NavLink className="nav-link" exact to="/config">Configuración</NavLink>
+                    <NavLink className="nav-link" to="/config">Configuración</NavLink>
                 </div>
             </div>
         </nav>
